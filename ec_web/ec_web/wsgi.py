fcfb1756616f6eeffe69f5624fa4bc85be344fce
Mrpatadiya <mrxmanscience@gmail.com>
2026-03-21 12:41:40 +0530

    created the first app caled sotre

---
+++ ec_web/ec_web/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ec_web project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ec_web.settings')
+
+application = get_wsgi_application()
