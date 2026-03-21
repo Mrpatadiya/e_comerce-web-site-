fb4397cc0259c59a9238e6e4fc85cce254cd2236
Mrpatadiya <mrxmanscience@gmail.com>
2026-03-22 00:07:44 +0530

    added database of render

--- ec_web/ec_web/settings.py
+++ ec_web/ec_web/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 from dotenv import load_dotenv
 import os
 
@@ -80,14 +81,7 @@ WSGI_APPLICATION = 'ec_web.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-   'default': {
-       'ENGINE': 'django.db.backends.postgresql',
-       'NAME': os.getenv('DB_NAME'),
-       'USER':os.getenv('DB_USER'),
-       'PASSWORD': os.getenv('DB_PASSWORD'),
-       'HOST': os.getenv('DB_HOST'),
-       'PORT': os.getenv('DB_PORT'),
-   }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
