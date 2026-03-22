0ff2200e622b2bcb5152acac6af67f7e5283df7d
Mrpatadiya <mrxmanscience@gmail.com>
2026-03-22 14:15:37 +0530

    chaged db into a safer version

--- ec_web/ec_web/settings.py
+++ ec_web/ec_web/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'ec_web.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    "default": dj_database_url.config(
+        default="sqlite:///db.sqlite3",
+        conn_max_age=600
+    )
 }
 
 
